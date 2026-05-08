3b169587eab0eac94c8c5c1a3a618e3394bde809
Westbrook-source <lordmacos7@gmail.com>
2026-05-08 13:06:17 +0000

    Refactor grading logic to use arrays and improve output
    Refactored the grading program to use an array for marks and added character variables for grade and remark. Improved output formatting and added comments for clarity.

--- MET253_Assignment_repo/Report_6313124.f90
+++ MET253_Assignment_repo/Report_6313124.f90
@@ -1,18 +1,28 @@
 Program Academic_report
 implicit none
-Integer, parameter:: maximum=100
-integer:: A
-integer:: i
-Do i=1,100
-IF(MARK >=80 .AND. MARK <=100)THEN
-PRINT*, 'A', 'distinction'
-ELSE IF(MARK >=60 .AND. MARK <=79) THEN
-PRINT*, 'B', 'credit'
-ELSE IF(MARK >=40 .AND. MARK <=59) THEN
-PRINT*, 'C', 'pass'
-ELSE IF(MARK >=0 .AND. MARK <=39) THEN
-PRINT*, 'F', 'fail'
-END IF 
+integer :: i
+real :: Marks(10)
+character(len=1) :: grade ! I add it to declare the character variable named "grade" and len=1 means it can only store one character.
+character(len=11) :: remark ! I add it to declare the character variable named "remark" and len=1 means it can only store 11 characters.
+Marks = (/85, 62, 45, 91, 38, 74, 55, 88, 61, 47/) !I added it to store values(students marks) into the Marks array.
+Print*, "Grade    Marks    Remarks"
+Print*, "------------------" ! I added it to print a line separator.
+Do i=1,10 ! I added it to start a loop i which will coun't from 1 to 10. this will make it process all 10 marks of stendents.
+if (Marks(i) >=80 .AND. Marks(i) <=100) then
+grade = "A"
+remark = "Distinction"
+else if (Marks(i) >=60 .AND. Marks(i) <=79) then
+grade = "B"
+remark = "Credit"
+else if (Marks(i) >=40 .AND. Marks(i) <=59) then
+grade = "C"
+remark = "Pass"
+else
+grade = "F"
+remark = "Fail"
+end if
+write(*,'(A1,2X,F5.1,2X,A)') grade, Marks(i), remark ! I added it for it to print values of grades, marks(i) and remark using specified format for spacing and alingment.
+end do
 END Program Academic_report
 !Lord Kwarteng Poku
 !6313124
